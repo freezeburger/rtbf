51e8dcb6dc8ea1b9333defea37c40466560a1afd
freezeburger <renaud.dubuis@decryptage.net>
2020-07-17 14:31:10 +0200

    Update

--- src/features/Login.vue
+++ src/features/Login.vue
@@ -1,51 +1,49 @@
 <template>
   <base-card title="Login" class="w-100">
-    
     <code>{{credentials}}</code>
 
-    <base-input 
-        v-model="credentials.email" 
-        :validator="validators.email">
-        <template #message>
-            Your Email
-        </template>
-        <template #invalided-message>
-            Use an <strong>@rtbf.be</strong> email.
-        </template>
+    <base-input v-model="credentials.email" :validator="validators.email">
+      <template #message>Your Email</template>
+      <template #invalided-message>
+        Use an
+        <strong>@rtbf.be</strong> email.
+      </template>
     </base-input>
 
     <base-input v-model="credentials.password">
-        <template #message>
-            Your Password
-        </template>
+      <template #message>Your Password</template>
     </base-input>
 
-    <base-button 
-        icon="🔑" 
-        variant="primary" 
-        size="large">Connect !</base-button>
-
+    <div @click="requestIdentification()">
+      <base-button icon="🔑" variant="primary" size="large">Connect !</base-button>
+    </div>
+    
   </base-card>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            credentials:{
-                email:'',
-                password:''
-            },
-            validators:{
-                email(value){
-                    return value.includes('@rtbf.be')
-                }
-            }
-        }
+  methods: {
+    requestIdentification() {
+      // KZK ?
+      alert(123);
     }
-}
+  },
+  data() {
+    return {
+      credentials: {
+        email: "",
+        password: ""
+      },
+      validators: {
+        email(value) {
+          return value.includes("@rtbf.be");
+        }
+      }
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
