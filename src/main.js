328fa5a241ed1f36dbbc993567dee44e56f3f296
freezeburger <renaud.dubuis@decryptage.net>
2020-07-17 16:03:23 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -12,3 +12,22 @@ new Vue({
   store,
   render: (h) => h(App)
 }).$mount('#app');
+
+
+//-------
+const URL = 'http://localhost:8000/auth/login'
+const options = {
+  method:'POST',
+  headers:{
+    'Content-Type': 'application/json'
+  },
+  body:JSON.stringify({
+    email:'bruno@email.com',
+    password:'bruno'
+  })
+}
+
+fetch(URL,options)
+  .then( res => res.json() )
+  .then( data => console.table(data) )
+
