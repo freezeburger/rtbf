87105becbccec5746bf008f407d04cee29c7417a
freezeburger <renaud.dubuis@decryptage.net>
2020-07-17 10:29:59 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    
+    <view-home></view-home>
   </div>
 </template>
 
 <script>
+import ViewHome from "./views/ViewHome";
+
 export default {
   name: "App",
-  methods:{
-    eventHandler(evt){
+  methods: {
+    eventHandler(evt) {
       console.warn(evt);
     }
   },
-  components: {}
+  components: {
+    ViewHome
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import BaseDemoSlot from './components/base/BaseDemoSlot.vue';
-import BaseDemoEvent from './components/base/BaseDemoEvent.vue';
-import BaseDemoClass from './components/base/BaseDemoClass.vue';
-
 import './auto-register';
 
 Vue.config.productionTip = false;
