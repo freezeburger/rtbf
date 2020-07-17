030632aa374728332b43ba498ebbc08694476922
freezeburger <renaud.dubuis@decryptage.net>
2020-07-17 12:15:56 +0200

    Nav

--- src/components/base/BaseNav.vue
+++ src/components/base/BaseNav.vue
@@ -1,37 +1,50 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          
-          <li class="nav-item" v-for=" route in routes" :key="route.text">
-            <a class="nav-link" v-bind:class="{ active: route.text==currentRoute.text}" style="height:50px" :href="route.link">{{route.text}}</a>
-          </li>
-           
-        </ul>
-      </div>
-    </nav>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+    <ul class="navbar-nav" v-if="routes">
+      <li class="nav-item" v-for="route in routes" :key="route.text">
+        <router-link
+          class="nav-link"
+          v-bind:class="{ active: route.link == currentRoute.path}"
+          style="height:50px"
+          :to="route.link"
+        >{{route.text}}</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-
-
 export default {
   name: "BaseNav",
   props: {
-    currentRoute:Object,
-    routes: {
+    /* routes: {
       validator: function(value) {
         return value.every(route => route.text && route.link);
       }
+    } */
+  },
+  computed: {
+    currentRoute() {
+      //console.log(this.$router.currentRoute);
+      return this.$router.currentRoute;
+    },
+    routes() {
+      const routes = this.$router.options.routes.map(route => ({
+        link: route.path,
+        text: route.name
+      }));
+      return routes;
     }
   },
-  // props: ['textsNav'],
-  components: {}
+  created() {
+    /*  this.$router.options.routes.push({
+    path: '/invited',
+    name: 'Invited',
+    component: ''
+  }) */
+    // console.table(this.$router.options.routes);
+  }
 };
-
-
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from '-./App.vue';
+import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './auto-register';
