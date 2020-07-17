59df8d0cc2bf7900b463e21f63a7ca8146424cb0
freezeburger <renaud.dubuis@decryptage.net>
2020-07-17 14:39:22 +0200

    VuX

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './auto-register';
 import router from './router';
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App)
 }).$mount('#app');
